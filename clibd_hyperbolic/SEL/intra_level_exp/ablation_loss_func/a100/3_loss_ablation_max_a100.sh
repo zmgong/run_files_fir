4f3dbfa783f3c7c9558f3593197f937d1d09f59a
zmgong <ming2280089874@gmail.com>
2026-03-17 14:27:03 -0700

    Add SLURM scripts for ablation loss functions on A100 and A40 configurations, including mean, weighted mean, max, sum, and combined loss strategies.

---
+++ clibd_hyperbolic/SEL/intra_level_exp/ablation_loss_func/a100/3_loss_ablation_max_a100.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH -J 3_loss_ablation_max_a100
+#SBATCH --gpus-per-node=h100:4
+#SBATCH --time=00:40:00
+#SBATCH --output=logs/%N-%j.out   # Terminal output to file named (hostname)-(jobid).out
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=48
+#SBATCH --mem=0
+#SBATCH --exclusive
+#SBATCH --account=rrg-msavva
+rsync -avhP ~/projects/rrg-msavva/zmgong/data/BIOSCAN_5M/BIOSCAN_5M.hdf5 $SLURM_TMPDIR/
+rsync -avhP ~/scratch/research/clibd_hyperbolic/data/BIOSCAN_5M/*.json $SLURM_TMPDIR/
+
+cd ~/scratch/research/clibd_hyperbolic
+module load python/3.11
+module load StdEnv/2023
+module load cuda/12.2
+module load faiss/1.7.4
+source ~/venvs/clibd-hyperbolic/bin/activate
+git checkout intra_ablation
+git pull
+pip install -e .
+
+export OMP_NUM_THREADS=12
+
+srun python scripts/train_cl.py 'model_config=for_bioscan_5m/hyperbolic/intra_level_exp/ablation_loss_func/a100/3_loss_ablation_max_a100.yaml' bioscan_5m_data.path_to_hdf5_data=$SLURM_TMPDIR/BIOSCAN_5M.hdf5
---
+++ clibd_hyperbolic/SEL/intra_level_exp/ablation_loss_func/a40/2_loss_ablation_weighted_mean_a40.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH -J 2_loss_ablation_weighted_mean_a40
+#SBATCH --gpus-per-node=nvidia_h100_80gb_hbm3_3g.40gb:4
+#SBATCH --time=00:50:00
+#SBATCH --output=logs/%N-%j.out   # Terminal output to file named (hostname)-(jobid).out
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=24
+#SBATCH --mem=256G
+#SBATCH --account=rrg-msavva
+
+rsync -avhP ~/projects/rrg-msavva/zmgong/data/BIOSCAN_5M/BIOSCAN_5M.hdf5 $SLURM_TMPDIR/
+rsync -avhP ~/scratch/research/clibd_hyperbolic/data/BIOSCAN_5M/*.json $SLURM_TMPDIR/
+
+cd ~/scratch/research/clibd_hyperbolic
+module load python/3.11
+module load StdEnv/2023
+module load cuda/12.2
+module load faiss/1.7.4
+source ~/venvs/clibd-hyperbolic/bin/activate
+git checkout intra_ablation
+git pull
+pip install -e .
+
+export OMP_NUM_THREADS=12
+
+srun python scripts/train_cl.py 'model_config=for_bioscan_5m/hyperbolic/intra_level_exp/ablation_loss_func/a40/2_loss_ablation_weighted_mean_a40.yaml' bioscan_5m_data.path_to_hdf5_data=$SLURM_TMPDIR/BIOSCAN_5M.hdf5
