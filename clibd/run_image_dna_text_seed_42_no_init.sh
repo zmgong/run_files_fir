ed5ade7cd898418db40f47845c9b542d5b043bda
zmgong <ming2280089874@gmail.com>
2026-03-30 19:54:22 -0700

    Update SLURM script for image DNA text training to use 1M dataset, adjust time limit to 24 hours, and change git branch for configuration management.

--- clibd/run_image_dna_text_seed_42_no_init.sh
+++ clibd/run_image_dna_text_seed_42_no_init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -J image_dna_text_no_init
 #SBATCH --gpus-per-node=h100:4
-#SBATCH --time=50:00:00
+#SBATCH --time=24:00:00
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
 #SBATCH --cpus-per-task=48
@@ -10,22 +10,21 @@
 #SBATCH --exclusive
 #SBATCH --account=rrg-msavva
 
-rsync -avhP ~/prrg-msavva/zmgong/data/BIOSCAN_5M/* $SLURM_TMPDIR/
-rsync -avhP ~/scratch/research/clibd_hyperbolic/data/BIOSCAN_5M/*.json $SLURM_TMPDIR/
+rsync -avhP ~/projects/rrg-msavva/zmgong/data/BIOSCAN_1M/* $SLURM_TMPDIR/
 
-cd ~/scratch/research/clibd_hyperbolic
+cd ~/scratch/research/clibd
 module load python/3.11
 module load StdEnv/2023
 module load cuda/12.2
 module load faiss/1.7.4
 module load arrow/21.0.0
 source ~/venvs/clibd-hyperbolic/bin/activate
-git checkout embed_table_and_debug_sel
+git checkout intra_ablation
 git pull
 pip install -e .
 
 export OMP_NUM_THREADS=12
 
-srun python scripts/train_cl.py 'model_config=for_bioscan_5m/final_experiments/image_dna_text_seed_42_no_init.yaml' bioscan_5m_data.path_to_hdf5_data=$SLURM_TMPDIR/BIOSCAN_5M.hdf5
-srun python scripts/inference_and_eval.py 'model_config=for_bioscan_5m/final_experiments/image_dna_text_seed_42_no_init.yaml' bioscan_5m_data.path_to_hdf5_data=$SLURM_TMPDIR/BIOSCAN_5M.hdf5 inference_and_eval_setting.eval_on=val
-srun python scripts/inference_and_eval.py 'model_config=for_bioscan_5m/final_experiments/image_dna_text_seed_42_no_init.yaml' bioscan_5m_data.path_to_hdf5_data=$SLURM_TMPDIR/BIOSCAN_5M.hdf5 inference_and_eval_setting.eval_on=test
+srun python scripts/train_cl.py 'model_config=for_bioscan_1m/final_experiments/image_dna_text_seed_42_no_init.yaml' bioscan_data.path_to_hdf5_data=$SLURM_TMPDIR/BioScan_data_in_splits.hdf5
+srun python scripts/inference_and_eval.py 'model_config=for_bioscan_1m/final_experiments/image_dna_text_seed_42_no_init.yaml' bioscan_data.path_to_hdf5_data=$SLURM_TMPDIR/BioScan_data_in_splits.hdf5 inference_and_eval_setting.eval_on=val
+srun python scripts/inference_and_eval.py 'model_config=for_bioscan_1m/final_experiments/image_dna_text_seed_42_no_init.yaml                                                                       ' bioscan_data.path_to_hdf5_data=$SLURM_TMPDIR/BioScan_data_in_splits.hdf5 inference_and_eval_setting.eval_on=test
